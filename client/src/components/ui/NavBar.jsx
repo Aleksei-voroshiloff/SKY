f788fa5c737bcddcceec6793f6d15eac0f040421
Aleksei-voroshiloff <alexey909@gmail.com>
2025-01-08 19:16:56 +0300

    client

---
+++ client/src/components/ui/NavBar.jsx
@@ -0,0 +1,59 @@
+import { Menu, MenuItem } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+export default function NavBar({ logoutHandler, user, handleItemClick, activeItem }) {
+  return (
+    <Menu pointing inverted>
+      <MenuItem name={user.data ? user.data.name : 'Гость'} />
+      {user.data && (
+        <MenuItem
+          as={Link}
+          to="/home"
+          name="Home"
+          active={activeItem === 'Home'}
+          onClick={() => handleItemClick('Home')}
+        />
+      )}
+
+      <Menu.Menu position="right">
+        {!user.data && (
+          <MenuItem
+            as={Link}
+            to="/signin"
+            name="Вход"
+            active={activeItem === 'Вход'}
+            onClick={() => handleItemClick('Вход')}
+          />
+        )}
+        {!user.data && (
+          <MenuItem
+            as={Link}
+            to="/signup"
+            name="Регистрация"
+            active={activeItem === 'Регистрация'}
+            onClick={() => handleItemClick('Регистрация')}
+          />
+        )}
+        {user.data && (
+          <>
+            <MenuItem
+              as={Link}
+              to="/redaction"
+              name="Редактировать трассу"
+              active={activeItem === 'Редактировать трассу'}
+              onClick={() => handleItemClick('Редактировать трассу')}
+            />
+            <MenuItem
+              as={Link}
+              to="/add"
+              name="Добавить трассу"
+              active={activeItem === 'Добавить трассу'}
+              onClick={() => handleItemClick('Добавить трассу')}
+            />
+            <MenuItem name="Выход" onClick={logoutHandler} />
+          </>
+        )}
+      </Menu.Menu>
+    </Menu>
+  );
+}
